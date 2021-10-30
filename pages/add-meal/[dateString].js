82cb604e619e94668e899a9237557a5879ab370e
hatertron3000 <patrickbuildscoolstuff@gmail.com>
2021-10-30 16:01:22 -0500

    date param on homepage

--- pages/add-meal/[dateString].js
+++ pages/add-meal/[dateString].js
@@ -67,7 +67,7 @@ export default function AddMeal({ isConnected, initialMeals }) {
     return isReady
         ? isConnected 
             ? <div>
-                <Link href="/"><button>&lt;&lt; Back to Meal Planner</button></Link>
+                <Link href={{pathname: '/', query: { date: dateString }}}><button>&lt;&lt; Back to Meal Planner</button></Link>
                 { <h1>{`Meal Plan for ${months[dateString.split('-')[1] - 1]} ${dateString.split('-')[2]}, ${dateString.split('-')[0]}`}</h1> }
                 <div>
                     {errors.map(error => <p>{error.message}</p>)}
